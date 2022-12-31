72794c7a33f694574c85add0c82c07ff06b09066
Meng Xiangzhuo <aumo@foxmail.com>
2023-01-01 07:31:41 +0800

    refactor: make readthedocs happy

--- binance_history/__init__.py
+++ binance_history/__init__.py
@@ -1,8 +1,14 @@
-from importlib import metadata
+import sys
 
 from .api import fetch_klines, fetch_agg_trades
 
+if sys.version_info[:2] >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
 __version__ = metadata.version(__package__)
-del metadata
+
+del metadata, sys
 
 __all__ = ["fetch_klines", "fetch_agg_trades"]
