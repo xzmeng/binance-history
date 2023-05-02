ada99dc03686c83aac65d77919cbe951895744bc
Meng Xiangzhuo <aumo@foxmail.com>
2023-05-03 02:39:53 +0800

    docs: update doc

--- binance_history/__init__.py
+++ binance_history/__init__.py
@@ -1,11 +1,8 @@
 import sys
 
-from .api import fetch_klines, fetch_agg_trades
+from .api import fetch_klines, fetch_agg_trades, fetch_data
 
-if sys.version_info[:2] >= (3, 8):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version(__package__)
 
